b65a2e5ef797745340b4056e830f7d523837cc92
CcMong <fullstackcodex@gmail.com>
2023-09-13 11:47:31 +0100

    Coded the footer component and its CSS page.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className={styles.container}>
+        <div>&copy;2023 Created by Charles MONG for AVAMAE.</div>
+        <div>Website Development by AVAMAE</div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
