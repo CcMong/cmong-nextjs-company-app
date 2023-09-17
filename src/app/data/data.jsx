ae3ba4ad95c3a6d4134854f44ea0f914bb86a995
CcMong <fullstackcodex@gmail.com>
2023-09-18 00:34:01 +0100

    Put all home page content into the data file, and referenced them accordingly.

--- src/app/data/data.jsx
+++ src/app/data/data.jsx
@@ -18,7 +18,29 @@ export const navlinks = [
 ];
 
 export const homePageContent = {
-
+    section2: {
+        heading: "Justo petentium te vix, scripta regione urbanitas",
+        body: "Populo facilisi nam no, dolor delenti deseruisse ne cum, nam quodsi aliquam eligendi ne. Ferri euismod accusata te nec, summo accumsan at vix. Ad vix legere impetus, nam consequat reformidans ut. No sit consul integre voluptatibus, omnuim lucilius ne mel. Et ancillae recteque deterruisset sed, ea nec odio option, ferri assum eum et.",
+        bullets: [
+        "Te pri efficiendi assueverit, id molestie suavitate per",
+        "Te nam dolorem rationibus repudiandae, ne ius falli aliquip consetetur",
+        "Ut qui dicant copiosae interpretaris",
+        "Ut indoctum patrioque voluptaria duo, ut vis semper abhorreant"
+        ]
+    },
+    section3: {
+        heading: "Nulla sem urna, dictum sed nisi in, viverra rutrum neque",
+        body: "Cras sit amet dapibus magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec finibus nulla quis lorem mollis lacinia. Fusce ut arcu ligula. Pellentesque augue ex, pellentesque ut maximus non, eleifend ut lorem. Vestibulum rutrum malesuada turpis, molestie mattis velit maximus ac. Quisque laculis hendrerit ex et tincidunt. Aenean eu magna ut nisi placerat fringilla in sed diam."
+    },
+    section4: {
+        heading: "Sed libero justo, lobortis sit amet suscipit non",
+        subheading: "taria duo ut vis semper abhorreant",
+        foretext: "Pellentesque ac condimentum felis. Suspendisse vel suscipit dolor, nec laoreet nulla. Nam auctor ultricies dapibus. Donec ac interdum dui, quis finibus lectus. Cras in ultrices neque. Curabitur eget turpis iaculis diam congue sagittis a vel ligula. Mauris ut arcu ex. Nullam quis orci ante. Nunc felis massa, posuere non gravida in, commodo in arcu. In feugiat magna non volutpat faucibus. Nam aliquam justo nec aliquam iaculis. Integer laoreet pulvinar elit pulvinar fermentum. Morbi vehicula sodales nunc ac varius. Proin porttitor porttitor libero vel pharetra.",
+        paragraphs: [
+            "Cras sit amet dapibus magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec magnis dis  parturient montes, nascetur ridiculus mus. Donec finibus nulla quis lorem mollis lacinia. Fusce ut arcu ligula. Pellentesque augue ex, pellentesque ut maximus non, eleifend ut lorem. Vestibulum rutrum malesuada turpis, molestie mattis velit maximus ac. Quisque laculis hendrerit ex et tincidunt. Aenean eu magna ut nisi placerat fringilla in sed diam. Suspendisse tristique vel dui nec imperdiet. Cras mattis ligula quis fermentum suscipit. Proin et elementum arcu, sit amet porttitor diam. Curabitur euismod, erat vitae tristique volutpat, augue lectus dignissim justo, at faucibus orci est a elit.",
+            "Sed sed sapien pretium, maximus felis vel, mollis elit. Sed libero justo, lobortis sit amet suscipit non, auctor non libero. Maecenas quis nisl eget enim porta b;andit a nec sapien. Mauris porttitor lorem ut egestas eusimod. Donec molestie tempor nibh, nec venenatis arcu ullamcorper sit amet. Nulla facilisi. Proin cursus neque ut tortor scelerisque, at iaculis nunc ornare. Pellentesque non nunc nulla. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean et sodales diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porttitor magna augue, non auctor quam placerat nec. Nulla sem urna, dictum sed nisi in, viverra rutrum neque. Aliquam ipsum nunc, porta a augue nec, fringilla mollis arcu. In a vehicula odio. Praesent vulputate turpis eleifend egestas ultrices. Prion nec sagittis nibh. Curabitur fringilla felis a porttitor maximus. Vestibulum aluquet ante nec leo malesuada porttitor sit amet et magna."
+        ]
+    }
 };
 
 export const aboutUsContent = {
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,8 +3,8 @@ import styles from './page.module.css';
 import Hero from '@/components/hero/Hero';
 import section2img from "../../public/learn_more_section.jpg";
 import Link from 'next/link';
-import section3img from "../../public/login_section.jpg";
 import modalImage from "../../public/company-logo.png";
+import { homePageContent } from './data/data';
 
 export default function Home() {
   
@@ -19,13 +19,16 @@ export default function Home() {
 
       <section className={styles.section2}>
         <div className={styles.learnContent}>
-          <h2 className={styles.learnTitle}>Justo petentium te vix, scripta regione urbanitas</h2>
-          <p className={styles.learnText}>Populo facilisi nam no, dolor delenti deseruisse ne cum, nam quodsi aliquam eligendi ne. Ferri euismod accusata te nec, summo accumsan at vix. Ad vix legere impetus, nam consequat reformidans ut. No sit consul integre voluptatibus, omnuim lucilius ne mel. Et ancillae recteque deterruisset sed, ea nec odio option, ferri assum eum et.</p>
+          <h2 className={styles.learnTitle}>
+            {homePageContent.section2.heading}
+          </h2>
+          <p className={styles.learnText}>
+            {homePageContent.section2.body}
+          </p>
           <ul className={styles.learnBullet}>
-            <li>Te pri efficiendi assueverit, id molestie suavitate per</li>
-            <li>Te nam dolorem rationibus repudiandae, ne ius falli aliquip consetetur</li>
-            <li>Ut qui dicant copiosae interpretaris</li>
-            <li>Ut indoctum patrioque voluptaria duo, ut vis semper abhorreant</li>
+            {homePageContent.section2.bullets.map((bullet) => (
+              <li>{bullet}</li>
+            ))}            
           </ul>
           <Link href="/contact-us">
             <button type='button' className={styles.learnButton}>Learn More</button>
@@ -47,8 +50,12 @@ export default function Home() {
         <div className={styles.panImgWrapper}>
           
           <div className={styles.loginContent}>
-            <h2 className={styles.loginTitle}>Nulla sem urna, dictum sed nisi in, viverra rutrum neque</h2>
-            <p className={styles.loginText}>Cras sit amet dapibus magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec finibus nulla quis lorem mollis lacinia. Fusce ut arcu ligula. Pellentesque augue ex, pellentesque ut maximus non, eleifend ut lorem. Vestibulum rutrum malesuada turpis, molestie mattis velit maximus ac. Quisque laculis hendrerit ex et tincidunt. Aenean eu magna ut nisi placerat fringilla in sed diam.</p>
+            <h2 className={styles.loginTitle}>
+              {homePageContent.section3.heading}
+            </h2>
+            <p className={styles.loginText}>
+              {homePageContent.section3.body}
+            </p>
             <button type="button" className={styles.loginButton}>Log in</button>
           </div>
         </div>         
@@ -56,19 +63,21 @@ export default function Home() {
       </section>
 
       <section className={styles.section4}>
-        <h2 className={styles.sec4Title}>Sed libero justo, lobortis sit amet suscipit non</h2>
-        <h3 className={styles.sec4Subtitle}>taria duo ut vis semper abhorreant</h3>
+        <h2 className={styles.sec4Title}>
+          {homePageContent.section4.heading}
+        </h2>
+        <h3 className={styles.sec4Subtitle}>
+        {homePageContent.section4.subheading}
+        </h3>
         <div className={styles.multicol}>
           <p className={styles.sec4Foretext}>
-             Pellentesque ac condimentum felis. Suspendisse vel suscipit dolor, nec laoreet nulla. Nam auctor ultricies dapibus. Donec ac interdum dui, quis finibus lectus. Cras in ultrices neque. Curabitur eget turpis iaculis diam congue sagittis a vel ligula. Mauris ut arcu ex. Nullam quis orci ante. Nunc felis massa, posuere non gravida in, commodo in arcu. In feugiat magna non volutpat faucibus. Nam aliquam justo nec aliquam iaculis. Integer laoreet pulvinar elit pulvinar fermentum. Morbi vehicula sodales nunc ac varius. Proin porttitor porttitor libero vel pharetra.
+            {homePageContent.section4.foretext}
           </p>
-          <p className={styles.sec4Text}>
-          Cras sit amet dapibus magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec magnis dis  parturient montes, nascetur ridiculus mus. Donec finibus nulla quis lorem mollis lacinia. Fusce ut arcu ligula. Pellentesque augue ex, pellentesque ut maximus non, eleifend ut lorem. Vestibulum rutrum malesuada turpis, molestie mattis velit maximus ac. Quisque laculis hendrerit ex et tincidunt. Aenean eu magna ut nisi placerat fringilla in sed diam. Suspendisse tristique vel dui nec imperdiet. Cras mattis ligula quis fermentum suscipit. Proin et elementum arcu, sit amet porttitor diam. Curabitur euismod, erat vitae tristique volutpat, augue lectus dignissim justo, at faucibus orci est a elit.
-          </p>
-          <p className={styles.sec4Text}>
-            Sed sed sapien pretium, maximus felis vel, mollis elit. Sed libero justo, lobortis sit amet suscipit non, auctor non libero. Maecenas quis nisl eget enim porta b;andit a nec sapien. Mauris porttitor lorem ut egestas eusimod. Donec molestie tempor nibh, nec venenatis arcu ullamcorper sit amet. Nulla facilisi. Proin cursus neque ut tortor scelerisque, at iaculis nunc ornare. Pellentesque non nunc nulla. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean et sodales diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porttitor magna augue, non auctor quam placerat nec. Nulla sem urna, dictum sed nisi in, viverra rutrum neque. Aliquam ipsum nunc, porta a augue nec, fringilla mollis arcu. In a vehicula odio. Praesent vulputate turpis eleifend egestas ultrices. Prion nec sagittis nibh. Curabitur fringilla felis a porttitor maximus. Vestibulum aluquet ante nec leo malesuada porttitor sit amet et magna.
-          </p>
-
+          {homePageContent.section4.paragraphs.map((paragraph) => (
+            <p className={styles.sec4Text}>
+              {paragraph}
+            </p>
+          ))}
         </div>
 
         <button className={styles.sec4Button}>Contact us</button>
